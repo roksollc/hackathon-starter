7dfa1a27dacbfd0c0ca6ecc12153a85e27f37153
Greg Baker <greg.baker@roksostudios.com>
2018-07-24 10:04:24 -0700

    Testing resumed

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '18.222.149.69',
+      host: '18.222.144.112',
       username: 'ubuntu',
       privateKey: 'hs-test-key.pem'
     })
